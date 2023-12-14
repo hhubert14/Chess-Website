a07db73697dd7af862aa6f771ffda8a2032c30fc
DRACula <c.daedalus@wustl.edu>
2023-12-14 15:09:18 -0600

    pirc defense knight move done

--- moves.js
+++ moves.js
@@ -1,15 +1,30 @@
-class Move{
-  public int start;
-  public int dest;
+knight = document.getElementById('g1')
+pawn = document.getElementById('f2')
+bishop = document.getElementById('c1')
+
+dot = document.createElement('td');
+img = new Image();
+img.src = "img/red_dot.jpg";
+dot.appendChild(img);
+
+blank = document.createElement('td');
+
+knight.addEventListener('click', knightMove, true);
+dot.addEventListener('click', knightGoto, true);
+
+function knightMove(){
+  console.log("clicked");
+  dest = document.getElementById('f3');                 //Support for later change to map implementation (i.e., possible destinations stored and retrieved from map queries)
+  dot.className = dest.className;
+  dot.id = dest.id;
+  dest.replaceWith(dot);  
 }
 
-class Tutorial_Piece{
-  public string name;
-  public tutorial_moves;
-  
-  constructor(name, tutorial_moves){
-    this.name = name;
-    this.tutorial_moves = tutorial_moves;
-  }
-  
+function knightGoto(){
+  knight_copy = knight.cloneNode(true);
+  knight_copy.className = dot.className;
+  knight_copy.id = dot.id;
+  dot.replaceWith(knight_copy);
+  blank.className = knight.className;
+  knight.replaceWith(blank);
 }
